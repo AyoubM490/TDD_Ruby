23fb4bae987c22d775a89c379fa06945e66af6fc
keza681 <lindakeza19@gmail.com>
2022-06-23 18:25:52 +0200

    Implement Fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -1,11 +1,30 @@
 class Solver
-  def initialize
+  def factorial(num)
+    i = 1
+    fact = 1
+    raise RuntimeError if num.negative?
+
+    while i <= num
+      fact *= i
+      i += 1
+    end
+
+    fact
   end
 
-  def factorial(number)
-    raise RangeError, 'Number should not be negative' if number.negative?
-    return 1 if [1, 0].include?(number)
+  def reverse(word)
+    word.reverse
+  end
 
-    number * factorial(number - 1)
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 5).zero?
+      'buzz'
+    elsif (num % 3).zero?
+      'fizz'
+    else
+      num.to_s
+    end
   end
 end
